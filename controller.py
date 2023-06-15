93fbe357fdf230f3f4799265dbe53c417f30689f
Ramil2503 <ramil.ax2503@gmail.com>
2023-06-15 21:19:40 +0400

    First commit

---
+++ controller.py
@@ -0,0 +1,42 @@
+from view import menu, show_contacts, print_message, input_contact, input_return, input_id
+import model
+from view import text
+
+
+def start():
+    while True:
+        choice = menu()
+        match choice:
+            case 1:
+                model.open_file()
+                print_message(text.open_successful)
+            case 2:
+                model.save_file()
+                print_message(text.file_saved)
+            case 3:
+                show_contacts(model.phone_book)
+            case 4:
+                new = input_contact(text.input_new_contact)
+                model.add_contact(new)
+                print_message(text.contact_saved(new.get('name')))
+            case 5:
+                word = input_return(text.search_word)
+                result = model.search(word)
+                show_contacts(result)
+            case 6:
+                word = input_return(text.search_word)
+                result = model.search(word)
+                show_contacts(result)
+                index = input_return(text.input_index)
+                new = input_contact(text.input_change_contact)
+                model.change(int(index), new)
+                old_name = model.phone_book[int(index)-1].get('name')
+                print_message(text.contact_changed(new.get('name') if new.get('name') else old_name))
+
+            case 7:
+                index = input_id(text.delete_contact)
+                model.delete(index - 1)
+                show_contacts(model.phone_book)
+                print_message(text.contact_deleted)
+            case 8:
+                break
