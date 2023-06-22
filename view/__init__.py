65d6b4b137ee77c756ef51a60f33ec1136a399b9
Ramil2503 <ramil.ax2503@gmail.com>
2023-06-22 13:25:10 +0400

    create-console-class

--- view/__init__.py
+++ view/__init__.py
@@ -1,4 +1,2 @@
-from .console import menu, show_contacts, show_contact, print_message, input_contact, input_return, input_id
+from .console import Console
 from .text import *
-
-__all__ = ['menu', 'show_contacts', 'input_contact', 'input_return']
